52387903ebbdcc49461b015e1f181bb146a464c2
Logan Cole <emtlogancole@gmail.com>
2022-12-06 12:51:32 -0600

    Add 4 methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,16 @@
 friends = ['Nate', 'Matt' , 'Adrian' , 'Bailey' , 'Kiana']
 numbers = [5, 6, 7, 8, 9]
 floats = [1.1, 2.2, 3.3, 4.4, 5.5]
-Booleans = [true, false, false, false]
+booleans = [true, false, false, false]
+
+# The unshift method adds something to the beginnning of an array. In this case, it adds Katrina to my list of friends. 
+friends.unshift('Katrina')
+
+# The shift method retrieves and removes the first item in an array. In this case, it does this with the number 5. 
+numbers.shift
+
+# The pop method removes the last element in an array and returns it. In this case, it's 5.5
+floats.pop
+
+#The push method adds an element to the end of an array. In this case, it adds True to the end. 
+booleans.push(true)
